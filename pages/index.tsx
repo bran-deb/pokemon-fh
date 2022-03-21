0cb2afda3bd055f4a19991c66b8b2c9371be828f
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-21 16:54:03 -0400

    feat(Root): implement GETSTATICPROPS function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,39 @@
-import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 import { Layout } from '../components/layouts'
 
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+
+  console.log({ props })
+
   return (
     <>
       <Layout title='listado de pokemons'>
-        <Button color='gradient'>
-          Hello world
-        </Button>
+        <ul>
+          <li>Pokemon</li>
+          <li>Pokemon</li>
+          <li>Pokemon</li>
+          <li>Pokemon</li>
+          <li>Pokemon</li>
+          <li>Pokemon</li>
+          <li>Pokemon</li>
+        </ul>
       </Layout>
     </>
   )
+}
+
+//TODO:
+// it runs on server side and only at build time and it is only used in pages
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  console.log('Hola mundo');
+
+  return {
+    props: {  //las props las manda al cliente
+      name: 'brandon'
+    }
+  }
 }
 
 export default HomePage
