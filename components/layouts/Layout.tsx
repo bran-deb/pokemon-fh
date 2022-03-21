220d9abcb6a06da5a56c980ec5e47089adc80e81
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-21 11:37:58 -0400

    feat(Root): add layout HomePage

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react"
+import Head from "next/head"
+
+interface Props {
+    title?: string;
+}
+
+export const Layout: FC<Props> = ({ children, title }) => {
+    return (
+        <>
+            <Head>
+                <title>{title || 'Pokemon App'}</title>
+                <meta name="author" content="Brandon Padilla" />
+                <meta name="description" content={`Informacion sobre pokemon ${title}`} />
+                <meta name="keywords" content={`${title},pokemon,pokedex`} />
+            </Head>
+
+            {/* Navbar */}
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
+import { Layout } from '../components/layouts'
 
 
 const HomePage: NextPage = () => {
   return (
     <>
-    <Button color='gradient'>
-      Hello world
-    </Button>
+      <Layout title='listado de pokemons'>
+        <Button color='gradient'>
+          Hello world
+        </Button>
+      </Layout>
     </>
   )
 }
