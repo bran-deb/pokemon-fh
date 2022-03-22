083a2d41f0e48aab07f677520171d4dd6b5bf211
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-21 20:01:44 -0400

    feat(Root): add pokemonCard component - add interface of typescript - add styles with next-ui

---
+++ components/pokemon/PokemonCard.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+import { Card, Grid, Row, Text } from "@nextui-org/react";
+
+import { SmallPokemon } from "../../interfaces";
+
+
+interface Props {
+    pokemon: SmallPokemon
+}
+
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
+    const { id, name, img, url } = pokemon
+
+    return (
+        <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+            <Card
+                hoverable
+                clickable
+            >
+
+                <Card.Body css={{ p: 1 }}>
+                    <Card.Image
+                        src={img}
+                        width='100%'
+                        height={140}
+                    />
+                </Card.Body>
+
+                <Card.Footer>
+                    <Row justify='space-between'>
+                        <Text transform='capitalize'>{name}</Text>
+                        <Text>{id}</Text>
+                    </Row>
+                </Card.Footer>
+
+            </Card>
+        </Grid>
+    )
+}
