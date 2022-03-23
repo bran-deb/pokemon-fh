77f98f174690bd67dcac4a8b297c252d750cf0ac
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-23 18:19:37 -0400

    feat(Root): verifica si el pokemon es favorito

--- utilities/favoritesStorage.utility.ts
+++ utilities/favoritesStorage.utility.ts
@@ -2,17 +2,28 @@
 
 const toggleFavorite = (id: number) => {
 
-    console.log('toggleFavorite llamado')
-
     let favorites: number[] = JSON.parse(localStorage.getItem('favorites') || '[]')
     //si existe el id lo quita del array
-    if(favorites.includes(id)){
-        favorites = favorites.filter(storageID =>storageID !==id)
-    }else{
+    if (favorites.includes(id)) {
+        favorites = favorites.filter(storageID => storageID !== id)
+    } else {
         favorites.push(id)
     }
-    localStorage.setItem('favorites',JSON.stringify(favorites))
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+}
+
+const existPokemon = (id: number): Boolean => {
+    //NOTE: para solucionar el error que da en el serverside y ejecuta solo el lado del cliente
+    if (typeof window === 'undefined') return false
+
+    const favorites: number[] = JSON.parse(localStorage.getItem('favorites') || '[]')
+
+    return favorites.includes(id)  //true/false
 }
 
 
-export default {toggleFavorite }
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    toggleFavorite,
+    existPokemon,
+}
