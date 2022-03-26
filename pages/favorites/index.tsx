23fba721f627c9a0b8c775aba530b6dba6fba4b2
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-26 00:59:39 -0400

    feat(Root): add favoriteCardPokemon component & refactor

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
 
-import { Card, Container, Grid, Image, Text } from "@nextui-org/react";
-
 import { Layout } from "../../components/layouts";
 import { NoFavoritos } from "../../components/ui";
 import { favoritesStorage } from '../../utilities';
-import { FavoritePokemons } from '../../components/pokemon/FavoritePokemons';
+import { FavoritePokemons } from '../../components/pokemon';
 
 const FavoritesPage = () => {
 
@@ -20,7 +18,7 @@ const FavoritesPage = () => {
             {
                 favoritePokemons.length === 0
                     ? (<NoFavoritos />)
-                    : (<FavoritePokemons favoritePokemons={favoritePokemons} />)
+                    : (<FavoritePokemons pokemons={favoritePokemons} />)
             }
         </Layout>
     )
