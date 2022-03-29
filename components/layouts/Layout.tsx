cf4ac28697ac7f28d939cd3ac7988fe299bafd25
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-29 12:06:08 -0400

    feat(Root): add meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,19 +1,28 @@
 import { FC } from "react"
 import Head from "next/head"
+
 import { Navbar } from '../ui';
+
+//NOTE: verificar el window en el backend
+const origin = (typeof window === 'undefined') ? '' : window.location.origin
 
 interface Props {
     title?: string;
 }
 
 export const Layout: FC<Props> = ({ children, title }) => {
+
     return (
         <>
             <Head>
                 <title>{title || 'Pokemon App'}</title>
                 <meta name="author" content="Brandon Padilla" />
                 <meta name="description" content={`Informacion sobre pokemon ${title}`} />
-                <meta name="keywords" content={`${title},pokemon,pokedex`} />
+                <meta name="keywords" content={`esta es la pagina sobre ${title}`} />
+
+                <meta property="og:title" content={`Informacion sobre ${title}`} />
+                <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
+                <meta property="og:image" content={`${origin}/img/banner.png`} />
             </Head>
 
             <Navbar />
